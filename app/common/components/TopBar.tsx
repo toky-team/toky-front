e1642c5b1e6d137e1552bfb1110a86a9c70e917e
halionaz <samhsj@korea.ac.kr>
2025-07-14 14:23:29 +0900

    feat: login header

---
+++ app/common/components/TopBar.tsx
@@ -0,0 +1,27 @@
+import Icon from '@/lib/assets/icons';
+import type { PropsWithChildren } from 'react';
+import { useNavigate } from 'react-router';
+
+interface Props extends PropsWithChildren {
+  hasHamburger?: boolean;
+}
+const TopBar = ({ hasHamburger = false, children }: Props) => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex h-[3.25rem] w-full items-center justify-between px-[1.25rem] py-[0.8125rem]">
+      <button onClick={() => navigate(-1)}>
+        <Icon.ArrowBack />
+      </button>
+      {children}
+      {/* TODO: 사이드바 메뉴 연결 */}
+      {hasHamburger ? (
+        <button>
+          <Icon.Hamburger />
+        </button>
+      ) : (
+        <span />
+      )}
+    </div>
+  );
+};
+export default TopBar;
---
+++ app/routes/login.tsx
@@ -0,0 +1,10 @@
+import TopBar from '@/common/components/TopBar';
+
+const Login = () => {
+  return (
+    <div>
+      <TopBar>로그인</TopBar>
+    </div>
+  );
+};
+export default Login;
