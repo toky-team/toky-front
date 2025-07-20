6b6ac59ef864ce6b74741ec2ef6dd12c5c851733
halionaz <samhsj@korea.ac.kr>
2025-07-20 16:35:18 +0900

    fix(design): youtube player 높이 조정

--- app/domain/live/components/Player/index.tsx
+++ app/domain/live/components/Player/index.tsx
@@ -1,19 +1,21 @@
 import ReactPlayer from 'react-player';
 
+import * as s from './style.css';
 interface Props {
   src: string;
 }
 const Player = ({ src }: Props) => {
   /**
    * TODO: 에러 해결
+   * 이승준 해결해줘 !!!!! ㅠㅠㅠ
    * Check the render method of `YoutubeVideoElement`.
    * See https://react.dev/link/warning-keys for more information.
    * Each child in a list should have a unique "key" prop.
    */
   return (
     <ReactPlayer
+      className={s.Container}
       src={src}
-      playing={true}
       style={{
         width: '100%',
         height: 'auto',
