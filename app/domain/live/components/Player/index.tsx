bb98266e3df2c9a6ad0eca13c62bc97490083d79
halionaz <samhsj@korea.ac.kr>
2025-08-20 23:46:58 +0900

    feat: live-url api 사용

--- app/domain/live/components/Player/index.tsx
+++ app/domain/live/components/Player/index.tsx
@@ -5,13 +5,6 @@ interface Props {
   src: string;
 }
 const Player = ({ src }: Props) => {
-  /**
-   * TODO: 에러 해결
-   * 이승준 해결해줘 !!!!! ㅠㅠㅠ
-   * Check the render method of `YoutubeVideoElement`.
-   * See https://react.dev/link/warning-keys for more information.
-   * Each child in a list should have a unique "key" prop.
-   */
   return (
     <div className={s.Wrapper}>
       <ReactPlayer className={s.Container} src={src} width="100%" height="100%" />
