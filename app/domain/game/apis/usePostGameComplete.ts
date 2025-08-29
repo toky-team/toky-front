a83ebb45e4355016174aa6d5a8db33532b4eedd1
halionaz <samhsj@korea.ac.kr>
2025-08-29 23:00:16 +0900

    fix(move)

---
+++ app/domain/game/apis/usePostGameComplete.ts
@@ -0,0 +1,17 @@
+import client from '@/common/utils/client';
+import { useMutation } from '@tanstack/react-query';
+
+interface PostGameCompleteRequest {
+  stage: 1 | 2;
+  win: boolean;
+}
+const postGameComplete = async (request: PostGameCompleteRequest) => {
+  const response = await client.post('/game/complete', request);
+  return response.data;
+};
+
+export const usePostGameComplete = () => {
+  return useMutation({
+    mutationFn: postGameComplete,
+  });
+};
