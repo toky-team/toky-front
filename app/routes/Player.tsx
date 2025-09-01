b1997448c2e6db5afc1d0b959d286df153755792
sjunee2 <tx0203@gmail.com>
2025-09-01 23:29:21 +0900

    feat(player): 플레이어 캐러셀 실제 데이터 삽입

--- app/routes/Player.tsx
+++ app/routes/Player.tsx
@@ -7,6 +7,11 @@ import FullPlayerView from "@/domain/player/components/FullPlayerView";
 import PrimaryPlayerView from "@/domain/player/components/PrimaryPlayerView"
 import { useGetPlayer } from "@/common/apis/useGetPlayer";
 import type { SportType } from "@/lib/types";
+import BannerSoccer from "@/lib/assets/images/banner_soccer.webp";
+import BannerBaseball from "@/lib/assets/images/banner_baseball.webp";
+import BannerBasketball from "@/lib/assets/images/banner_basketball.webp";
+import BannerRugby from "@/lib/assets/images/banner_rugby.webp";
+import BannerIcehockey from "@/lib/assets/images/banner_hockey.webp";
 
 // UI 표시용 SportsTab을 API용 SportType으로 변환
 const sportsTabToSportType = (tab: SportsTab): SportType => {
@@ -24,10 +29,8 @@ const Player = () => {
   const [currentSport, setCurrentSport] = useState<SportType>("야구");
   const [displayTab, setDisplayTab] = useState<Exclude<SportsTab, "전체">>("야구");
 
-  const tempSlides = [
-    { id: '1', image: "https://placehold.co/600x200", alt: '슬라이드 1' },
-    { id: '2', image: "https://placehold.co/600x200", alt: '슬라이드 2' },
-    { id: '3', image: "https://placehold.co/600x200", alt: '슬라이드 3' }
+  const slides = [
+    { id: '1', image: "", alt: '슬라이드 1' },
   ]; 
 
   const { data: koreaPlayers = [] } = useGetPlayer(currentSport, "고려대학교");
@@ -46,13 +49,25 @@ const Player = () => {
     }
   };
 
+  if (currentSport === "축구") {
+    slides[0].image = BannerSoccer;
+  } else if (currentSport === "야구") {
+    slides[0].image = BannerBaseball;
+  } else if (currentSport === "농구") {
+    slides[0].image = BannerBasketball;
+  } else if (currentSport === "럭비") {
+    slides[0].image = BannerRugby;
+  } else if (currentSport === "아이스하키") {
+    slides[0].image = BannerIcehockey;
+  }
+
   return (
     <>
       <MainTopBar />
       <NavBar />
       <SportsNavBar viewAll={false} value={displayTab} onChange={handleTabChange} />
       <div className="w-full flex flex-col gap-7">
-        <CustomCarousel slides={tempSlides} indicator="dots" />
+        <CustomCarousel slides={slides} indicator="dots" />
         <PrimaryPlayerView primaryPlayers={primaryPlayers} />
         <FullPlayerView koreaPlayers={koreaPlayers} yonseiPlayers={yonseiPlayers} />
       </div>
