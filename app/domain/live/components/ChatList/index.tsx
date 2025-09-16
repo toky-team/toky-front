6243d69c0ee3886002ee22da52b1ff3418077d05
halionaz <samhsj@korea.ac.kr>
2025-09-16 15:32:32 +0900

    feat: 채팅 검열

--- app/domain/live/components/ChatList/index.tsx
+++ app/domain/live/components/ChatList/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import * as s from './style.css';
 
--- app/domain/live/hooks/useSportSocket.ts
+++ app/domain/live/hooks/useSportSocket.ts
@@ -18,12 +18,26 @@ const useSportSocket = (sport: SportType) => {
       openToast({ message: '라이브 채팅 연결에 실패했어요' });
     };
 
+    const onFiltered = ({
+      filteredMessage,
+    }: {
+      filteredMessage: {
+        id: string;
+        sport: SportType;
+      };
+    }) => {
+      if (sport !== filteredMessage.sport) return;
+      setNewMessages((prev) => prev.filter((message) => message.id !== filteredMessage.id));
+    };
+
     chatSocket.on('receive_message', onReceiveMessage);
+    chatSocket.on('message_filtered', onFiltered);
     chatSocket.on('connect_error', onConnectError);
     chatSocket.emit('join_room', { sport });
 
     return () => {
       chatSocket.off('receive_message', onReceiveMessage);
+      chatSocket.off('message_filtered', onFiltered);
       chatSocket.off('connect_error', onConnectError);
       chatSocket.emit('leave_room', { sport });
     };
