98cb48aa9d511d48cacab4434276727dd3c8cf01
halionaz <samhsj@korea.ac.kr>
2025-09-19 10:11:43 +0900

    feat: chat 색깔

--- app/domain/live/components/Chat/index.tsx
+++ app/domain/live/components/Chat/index.tsx
@@ -1,13 +1,15 @@
+import type { UniversityType } from '@/lib/types';
 import * as s from './style.css';
 
 interface Props {
   nickname: string;
   message: string;
+  university: UniversityType;
 }
-const Chat = ({ nickname, message }: Props) => {
+const Chat = ({ nickname, message, university }: Props) => {
   return (
     <div className={s.Container}>
-      <div className={s.Nickname}>{nickname}</div>
+      <div className={s.Nickname({ university })}>{nickname}</div>
       <div className={s.Message}>{message}</div>
     </div>
   );
